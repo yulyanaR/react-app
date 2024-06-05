4f6d2ad9112222b53a593b98013a0f52d4552e36
Yulyana Ramanenka <yul.ramanenka@gmail.com>
2024-06-05 20:31:17 +0100

    Added MUI and theming

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,39 @@
 import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import theme from './theme/theme';
+
+import Layout from './components/Layout';
+
 //import pages
 import List from './pages/List';
 import Add from './pages/Add';
 import Update from './pages/Update';
 import NotFound from './pages/NotFound';
 
-import Layout from './components/Layout';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
 
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<List />} />
-          <Route path="/add" element={<Add />} />
-          <Route path="/update" element={<Update />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<List />} />
+            <Route path="/add" element={<Add />} />
+            <Route path="/update" element={<Update />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
     </Router>
   )
 }
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 function List() {
     return (
         <>
             <div>List</div>
+            <Button variant="text">Text</Button>
+            <Button variant="contained">Contained</Button>
+            <Button variant="outlined">Outlined</Button>
         </>
     );
 }
